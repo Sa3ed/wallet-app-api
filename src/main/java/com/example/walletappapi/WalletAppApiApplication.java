75b5a908e5da8af0c7112c40e960692817d05db8
Sa3ed Mostafa <sa3ed.mostafa@hotmail.com>
2024-01-14 18:43:27 +0200

    initial

---
+++ src/main/java/com/example/walletappapi/WalletAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/walletappapi/WalletAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
